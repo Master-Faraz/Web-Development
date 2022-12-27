48c9d59a3124de54d6096368e0a72f187cd6743d
Predator <farazali9028@gmail.com>
2022-12-27 21:48:12 +0530

    JS lec 16

---
+++ JavaScript/first.js
@@ -0,0 +1,40 @@
+const pi = 3.14;
+
+var first_name = "Faraz";
+var middle_name = "Ali";
+var last_name = "Ahmad";
+
+var age = 23;
+
+var cgpa = 8.17;
+
+console.log(`
+    Name is : ${first_name}  ${middle_name}  ${last_name}
+    Age is : ${age}
+    CGPA : ${cgpa}
+`)
+
+console.log("Full name is " + first_name + " " + middle_name + " " + last_name);
+
+
+
+//********************************************** Discount Calculator */
+
+const mrp = 1000, current_price = 151;
+
+var discount = 75;
+
+var Selling_Price = (mrp - (mrp * discount) / 100);
+
+
+console.log("\nYour Selling Price is " + Selling_Price);
+
+//      Converting current price to discount percentage 
+
+discount_percentage = ((mrp - current_price) / mrp) * 100;
+
+console.log(discount_percentage);
+
+discount_percentage = Math.round(discount_percentage);   //.            For Rounding the value
+
+console.log(discount_percentage);
