2a46a6ba27a80466ecc239d0ff8ac96f32f68a02
Predator <farazali9028@gmail.com>
2022-12-31 23:42:55 +0530

    last day of year

--- JavaScript/first.js
+++ JavaScript/first.js
@@ -1,54 +1,54 @@
-// const pi = 3.14;
+const pi = 3.14;
 
-// var first_name = "Faraz";
-// var middle_name = "Ali";
-// var last_name = "Ahmad";
+var first_name = "Faraz";
+var middle_name = "Ali";
+var last_name = "Ahmad";
 
-// var age = 23;
+var age = 23;
 
-// var cgpa = 8.17;
+var cgpa = 8.17;
 
-// console.log(`
-//     Name is : ${first_name}  ${middle_name}  ${last_name}
-//     Age is : ${age}
-//     CGPA : ${cgpa}
-// `)
+console.log(`
+    Name is : ${first_name}  ${middle_name}  ${last_name}
+    Age is : ${age}
+    CGPA : ${cgpa}
+`)
 
-// console.log("Full name is " + first_name + " " + middle_name + " " + last_name);
-
-
-
-// //********************************************** Discount Calculator */
-
-// const mrp = 1000, current_price = 151;
-
-// var discount = 75;
-
-// var Selling_Price = (mrp - (mrp * discount) / 100);
+console.log("Full name is " + first_name + " " + middle_name + " " + last_name);
 
 
+
+//********************************************** Discount Calculator */
+
+const mrp = 1000, current_price = 151;
+
+var discount = 75;
+
+var Selling_Price = (mrp - (mrp * discount) / 100);
 
 
 
 
-// console.log("\nYour Selling Price is " + Selling_Price);
-
-// //      Converting current price to discount percentage 
-
-// discount_percentage = ((mrp - current_price) / mrp) * 100;
-
-// console.log(discount_percentage);
-
-// discount_percentage = Math.round(discount_percentage);   //.            For Rounding the value
-
-// console.log(discount_percentage);
 
 
-// function fun(a, b) {
-//     if (a > b)
-//         return a;
-//     return b;
-// }
+console.log("\nYour Selling Price is " + Selling_Price);
 
-// var y = fun(6, 50)
-// console.log(y)
+//      Converting current price to discount percentage 
+
+discount_percentage = ((mrp - current_price) / mrp) * 100;
+
+console.log(discount_percentage);
+
+discount_percentage = Math.round(discount_percentage);   //.            For Rounding the value
+
+console.log(discount_percentage);
+
+
+function fun(a, b) {
+    if (a > b)
+        return a;
+    return b;
+}
+
+var y = fun(6, 50)
+console.log(y)
