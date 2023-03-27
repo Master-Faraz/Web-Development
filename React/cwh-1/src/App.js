5f83763b75ba87712946e57516d8b69ffe723722
Predator <farazali9028@gmail.com>
2023-03-28 04:58:20 +0530

    Added Dark Mode

--- React/cwh-1/src/App.js
+++ React/cwh-1/src/App.js
@@ -14,12 +14,6 @@ function App() {
       </div>
 
       
-      <Navbar title="TextUtils" prop1="My Home" prop2="About US" />
-      {/* container class in bootstrap for alligning in center  and my-3 -> margin 3 in y*/}
-      <div className="container my-3">
-        <TextForm heading="Enter the text to analize" />
-
-      </div>
     </>
   );
 }
--- React/cwh-1/src/Components/TextForm.js
+++ React/cwh-1/src/Components/TextForm.js
@@ -1,7 +1,13 @@
 import React, { useState } from 'react'
 
 function TextForm(props) {
-    const [text, SetText] = useState(" ");
+    const [text, SetText] = useState("");
+    const [Email, SetEmail] = useState([]);
+    const [theme, SetTheme] = useState({
+        backgroundColor: "white",
+        color: "black"
+    });
+    const [btn_Theme, ToggleTheme] = useState("Enable Dark Mode")
 
     const handleUP = () => {
         console.log("Button clicked")
@@ -14,8 +20,48 @@ function TextForm(props) {
     }
 
 
-    const handleDown = ()=>{
+    const handleDown = () => {
         SetText(text.toLowerCase())
+
+    }
+
+    const handleClear = () => {
+        let new_text = ""
+        SetText(new_text);
+    }
+
+    const handleEmail = () => {
+        var email_arr = text.match(/[\w\d.-]+@[\w\d.-]+/g)       //.        Regular Expression for email extraction
+        SetEmail(email_arr);
+
+    }
+
+    const handleCapitalize = () => {
+        const nameCapitalized = text.charAt(0).toUpperCase() + text.slice(1);
+        SetText(nameCapitalized)
+    }
+    const handleTitleCase = () => {
+        let new_text = text.replace(/\b[a-z]/g, (x) => x.toUpperCase())
+        SetText(new_text)
+    }
+
+    const handleTheme = () => {
+        let x = btn_Theme
+
+        if (x === "Enable Dark Mode") {
+            ToggleTheme("Enable Light Mode")
+            SetTheme({
+                backgroundColor: "black",
+                color: "white"
+            })
+        }
+        else {
+            ToggleTheme("Enable Dark Mode")
+            SetTheme({
+                backgroundColor: "white",
+                color: "black"
+            })
+        }
 
     }
 
@@ -24,25 +70,57 @@ function TextForm(props) {
         borderRadius: "10px",
         color: "white",
 
-        background: "rgb(2,0,36)",
+        // background: "rgb(2,0,36)",
         background: "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(26,26,187,1) 2%, rgba(9,15,125,1) 5%, rgba(0,212,255,1) 100%)",
-        margin : "0px 20px"
+        // margin: "0px 20px"
     }
- 
+
+    // const ThemeColor = {
+    //     backgroundColor: "black",
+    //     color: "white"
+    // }
 
     return (
-        <>
-            <h1>{props.heading}</h1>
+        <div style={theme}>
+            {/************************************************      whole textbox     ******************************************************/}
             <div className="mb-3">
+                <h1>{props.heading}</h1>
                 {/* <label for="myBox" className="form-label"></label> */}
-                <textarea className="form-control" id="exampleFormControlTextarea1" value={text} onChange={handleUPONchange} rows="8"></textarea>
+                <textarea className="form-control" value={text} onChange={handleUPONchange} rows="8" style={theme}></textarea>
 
             </div>
+            {/* //.*******************************************      Buttons     ***************************************** */}
 
-            <button className="uppercase" style={buttonStyle} onClick={handleUP}>Convert to uppercase</button>
-            <button className="lowercase" style={buttonStyle} onClick={handleDown}>Convert to Lowercase</button>
-         
-        </>
+            <button className="uppercase " style={buttonStyle} onClick={handleUP}>Convert to uppercase</button>
+            <button className="lowercase mx-2" style={buttonStyle} onClick={handleDown}>Convert to Lowercase</button>
+            <button className=" mx-2" style={buttonStyle} onClick={handleClear}>Clear Text</button>
+            <button className=" mx-2" style={buttonStyle} onClick={handleEmail}>Email Extractor</button>
+            <button className=" mx-2" style={buttonStyle} onClick={handleCapitalize}>Capitalize</button>
+            <button className=" mx-2" style={buttonStyle} onClick={handleTitleCase}>Title Case</button>
+
+            {/* //.******************************************************************************************************* */}
+
+            <div className="container my-4">
+
+                <h2>Your Text Summary</h2>
+                <p>{text.split(" ").length} words and {text.length} characters </p>
+            </div>
+
+            <div className="container my-4">
+                <h2>Emails </h2>
+                <p> {Email.map((email, index) => <li key={index}>{email}</li>)}</p>
+            </div>
+
+            <div className="container my-4">
+                <h2> Preview </h2>
+                <p>{text}</p>
+            </div>
+
+            <div className="container my-4">
+                <button className='my-3' style={buttonStyle} onClick={handleTheme}>{btn_Theme}</button>
+            </div>
+
+        </div>
     )
 }
 
