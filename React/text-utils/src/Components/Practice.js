efea6dade7935d696e9328b0ba820f24cac8e351
Predator <farazali9028@gmail.com>
2023-04-07 23:42:10 +0530

    .

--- React/text-utils/src/Components/Practice.js
+++ React/text-utils/src/Components/Practice.js
@@ -8,6 +8,7 @@ function Practice() {
             <ul>
                 <li><Link to="/" >  Home  </Link>   </li>
                 <li><Link to="/about" >  About  </Link>   </li>
+                <li><Link to="/about" >  About  </Link>   </li>
             </ul>
 
         </div>
