d4ab033a143b4bd1794c2f5341ed01215c27a53e
Predator <farazali9028@gmail.com>
2023-06-25 22:09:03 +0530

    Added infinite scroll and loading Bar

--- React/newsapk/src/App.js
+++ React/newsapk/src/App.js
@@ -10,33 +10,50 @@ import {
   Routes,
 } from "react-router-dom";
 
+import LoadingBar from 'react-top-loading-bar';
+
+
 export default class App extends Component {
   pageSize = 6;
+  
+  state = {
+    progress: 0
+  }
 
+  setProgress = (progress) => {
+    this.setState({ progress: progress })
+  }
 
   render() {
+
     return (
-      <div>
+      <>
         <BrowserRouter>
-
           <Navbar />
-          
 
-          <Routes>
-            <Route exact path="/" element={<News key="general" pageSize={this.pageSize} country="in" category="general" />} />
-            <Route exact path="/general" element={<News key="general" pageSize={this.pageSize} country="in" category="general" />} />
-            <Route exact path="/business" element={<News key="business" pageSize={this.pageSize} country="in" category="business" />} />
-            <Route exact path="/entertainment" element={<News key="entertainment" pageSize={this.pageSize} country="in" category="entertainment" />} />
-            <Route exact path="/health" element={<News key="health" pageSize={this.pageSize} country="in" category="health" />} />
-            <Route exact path="/science" element={<News key="science" pageSize={this.pageSize} country="in" category="science" />} />
-            <Route exact path="/sports" element={<News pageSize={this.pageSize} key="sports" country="in" category="sports" />} />
-            <Route exact path="/technology" element={<News key="technology" pageSize={this.pageSize} country="in" category="technology" />} />
+
+          <LoadingBar
+          height={3}
+            color='#f11946'
+            progress={this.state.progress}
+           
+          />
+
+          <Routes >
+            <Route exact path="/" element={<News setProgress={this.setProgress} key="general" pageSize={this.pageSize} country="in" category="general" />} />
+            <Route exact path="/general" element={<News setProgress={this.setProgress} key="general" pageSize={this.pageSize} country="in" category="general" />} />
+            <Route exact path="/business" element={<News setProgress={this.setProgress} key="business" pageSize={this.pageSize} country="in" category="business" />} />
+            <Route exact path="/entertainment" element={<News setProgress={this.setProgress} key="entertainment" pageSize={this.pageSize} country="in" category="entertainment" />} />
+            <Route exact path="/health" element={<News setProgress={this.setProgress} key="health" pageSize={this.pageSize} country="in" category="health" />} />
+            <Route exact path="/science" element={<News setProgress={this.setProgress} key="science" pageSize={this.pageSize} country="in" category="science" />} />
+            <Route exact path="/sports" element={<News setProgress={this.setProgress} key="sports" pageSize={this.pageSize} country="in" category="sports" />} />
+            <Route exact path="/technology" element={<News setProgress={this.setProgress} key="technology" pageSize={this.pageSize} country="in" category="technology" />} />
             {/* Here we use Key which is unique so that the apk re-renders the whole component on changing tab */}
 
           </Routes>
         </BrowserRouter>
 
-      </div>
+      </>
     )
   }
 }
