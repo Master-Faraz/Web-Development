2a02028d101d25fd7097042ec031785c1793ed36
Predator <farazali9028@gmail.com>
2023-07-15 22:07:35 +0530

    blood bank component updated

--- React/donate-life/src/components/Find_Blood_Bank.js
+++ React/donate-life/src/components/Find_Blood_Bank.js
@@ -2,19 +2,22 @@ import React from 'react'
 import Internal_Dropdown from './Internal_Dropdown'
 import Horizontal_Card from './Horizontal_Card'
 
+import bb from "./Images/bb.jpg"
+
 
 function Find_Blood_Bank() {
     return (
         <div className="container-fluid" style={{ height: "100vh", width: "70vw", marginTop: "65px" }}>
 
-        <Internal_Dropdown name="Find Donor" dd1="Select State" dd2="Select City" dd3="Blood Group" />
+            <Internal_Dropdown name="Find Donor" dd1="Select State" dd2="Select City" dd3="Blood Group" />
 
-        <div className="container-fluid row mx-2 my-5" >
-            <Horizontal_Card />
+            <div className="container-fluid row mx-2 my-5" >
+                <Horizontal_Card img={bb} name="Blood Bank A" contact="8210189562" add="Phulwari Sharif, Patna -801505"
+                blood={[1,2,3,4,5,6,7,8]} />
+            </div>
+
+
         </div>
-
-
-    </div>
     )
 }
 
--- React/donate-life/src/components/Horizontal_Card.js
+++ React/donate-life/src/components/Horizontal_Card.js
@@ -1,25 +1,75 @@
 import React from 'react'
-import bb from "./Images/bb.jpg"
-
-function Horizontal_Card() {
+function Horizontal_Card(props) {
     return (
-        <div class="card mb-3" style={{ width: "100%", height: "279px" }}>
-            <div class="row g-0">
-                <div class="col-md-4">
-                    <img src={bb} class="img-fluid rounded-start" alt="..." style={{ height: "279px", width: "453px", borderRadius: "40px 0px 0px 40px" }} />
+
+        <div className="container-fluid" style={{ width: "100%", height: "280px", borderRadius: "40px 0px 0px 40px" }}>
+            <div className="row " style={{ borderRadius: "40px 0px 0px 40px", background: "linear-gradient(180deg, #D9D9D9 52.97%, rgba(217, 217, 217, 0.00) 100%)" }}>
+                {/* Card Image div */}
+                <div className="col-md-4">
+                    <img src={props.img} className="img-fluid" alt="..." style={{ height: "280px", width: "100%", borderRadius: "40px 0px 0px 40px", margin: "0px", padding: "0px" }} />
                 </div>
-                <div class="col-md-8" style={{
-                    background: "linear-gradient(180deg, #D9D9D9 52.97%, rgba(217, 217, 217, 0.00) 100%)"
-                }}>
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+
+                {/* Card description div */}
+                <div className="col-8  ">
+                    {/* Here i am dividing div into two halfs */}
+                    <div className="row">
+                        {/* First half contains blood bank Name, contact and address */}
+                        <div className="col-12 " style={{ height: "140px" }}>
+                            <div className="row" style={{ height: "70px" }}>
+                                <div className="col-9 " > {props.name}</div>
+                                <div className="col-3 "> {props.contact}</div>
+                            </div>
+
+                            <div className="row" style={{ height: "70px" }}>
+                                <div className="col-12 " >{props.add}</div>
+                            </div>
+                        </div>
+
+                        {/* The Second half contains the BLOOD AVAILIBILITY status */}
+                        <div className="col-12" style={{ height: "140px" }}>
+
+                            <div className="row" style={{ height: "35px" }}>
+                                <div className="col"> Blood Availibility</div>
+                            </div>
+
+                            <div className="row" style={{ height: "105px" }}>
+                                <div className="col-3">A+  : {props.blood[0]} units</div>
+                                <div className="col-3">A-  : {props.blood[1]} units</div>
+                                <div className="col-3">B+  : {props.blood[2]} units</div>
+                                <div className="col-3">B-  : {props.blood[3]} units</div>
+                                <div className="col-3">AB+  : {props.blood[4]} units</div>
+                                <div className="col-3">AB-  : {props.blood[5]} units</div>
+                                <div className="col-3">O+  : {props.blood[6]} units</div>
+                                <div className="col-3">O-  : {props.blood[7]} units</div>
+                            </div>
+
+                        </div>
                     </div>
+
                 </div>
             </div>
         </div>
+
     )
 }
 
 export default Horizontal_Card
+
+
+
+{/* <div className="card mb-3 container-fluid" style={{ width: "100%", height: "279px", background: "red" }}>
+<div className="row g-0 " style={{ background: "yellow" }}>
+    <div className="col-md-4">
+        <img src={bb} className="img-fluid rounded-start" alt="..." style={{ height: "279px", width: "453px", borderRadius: "40px 0px 0px 40px", margin: "0px", padding: "0px" }} />
+    </div>
+    <div className="col-md-8" style={{
+        background: "linear-gradient(180deg, #D9D9D9 52.97%, rgba(217, 217, 217, 0.00) 100%)" , background:"green"
+    }}>
+        <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+        </div>
+    </div>
+</div>
+</div> */}
